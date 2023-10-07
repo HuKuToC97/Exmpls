2d523ff614c600b7a0e564e98ea2cc0a87338e4c
NiKiTa <hukuta199701@gmail.com>
2023-10-07 18:23:08 +0300

    Singl work in Examle009

--- Examples008 v.2/Program.cs
+++ Examples008 v.2/Program.cs
@@ -1,4 +1,12 @@
-﻿int a1 = new Random().Next(1, 5);
+﻿int Max(int arg1, int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2> result ) result = arg2;
+    if(arg3> result ) result = arg3;
+    return result;
+}
+
+int a1 = new Random().Next(1, 5000);
 System.Console.Write(a1 + ", ");
 int a2 = new Random().Next(1, 5);
 System.Console.Write(a2 + ", ");
@@ -14,19 +22,17 @@ int c1 = new Random().Next(1, 5);
 System.Console.Write(c1 + ", ");
 int c2 = new Random().Next(1, 5);
 System.Console.Write(c2 + ", ");
-int c3 = new Random().Next(1, 5);
+int c3 = new Random().Next(1, 3000);
 System.Console.WriteLine(c3 + ", ");
 
-int max = a1;
-if(b1> max ) max = b1;
-if(c1> max ) max = c1;
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max( Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
-if(a2> max ) max = a2;
-if(b2> max ) max = b2;
-if(c2> max ) max = c2;
-
-if(a3> max ) max = a3;
-if(b3> max ) max = b3;
-if(c3> max ) max = c3;
 
 System.Console.WriteLine(max);
---
+++ Examples009_/Program.cs
@@ -0,0 +1,38 @@
+﻿int Max(int arg1, int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2> result ) result = arg2;
+    if(arg3> result ) result = arg3;
+    return result;
+}
+
+int a1 = new Random().Next(1, 5000);
+System.Console.Write(a1 + ", ");
+int a2 = new Random().Next(1, 5);
+System.Console.Write(a2 + ", ");
+int a3 = new Random().Next(1, 5);
+System.Console.Write(a3 + ", ");
+int b1 = new Random().Next(1, 5);
+System.Console.Write(b1 + ", ");
+int b2 = new Random().Next(1, 5);
+System.Console.Write(b2 + ", ");
+int b3 = new Random().Next(1, 5);
+System.Console.Write(b3 + ", ");
+int c1 = new Random().Next(1, 5);
+System.Console.Write(c1 + ", ");
+int c2 = new Random().Next(1, 5);
+System.Console.Write(c2 + ", ");
+int c3 = new Random().Next(1, 3000);
+System.Console.WriteLine(c3 + ", ");
+
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max( Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+
+System.Console.WriteLine(max);
