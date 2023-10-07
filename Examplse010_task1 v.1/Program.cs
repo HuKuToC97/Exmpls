cb581d41c5c92ead418b7e6a2d8469eaf5e0fa2c
NiKiTa <hukuta199701@gmail.com>
2023-10-07 19:13:33 +0300

    Singl work in Examle010v2

--- Examplse010_task1 v.1/Program.cs
+++ Examplse010_task1 v.1/Program.cs
@@ -1,13 +1,61 @@
-﻿int[] array = { 1, 2, 3, 4, 5, 6, 4, 8, 9 };
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
 
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position + ", "]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+System.Console.Write("Позицию какого числа найти? Введите число=>");
+int find = System.Console.Readline()
+int pos = IndexOf(array, )
 int n = array.Length;
+
+
+
 int find = 4;
 
 int index = 0;
 
 while (index < n)
 {
-    if(array[index] == find)
+    if (array[index] == find)
     {
         System.Console.WriteLine(index);
         break;
